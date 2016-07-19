8e2207d3f46eb5112d3ac5e66de7c1a58e8c557b
Van Le <singularity@vannio.co.uk>
2016-07-19 14:57:30 +0100

    Joining rooms! Yay

--- server.js
+++ server.js
@@ -7,12 +7,34 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  socket.on('hostGame', function(challengeID){
+    var roomID = 1;
 
-  socket.on('startGame', function(data){
-    console.log(data);
+    socket.join(roomID, function(){
+      socket.broadcast.emit('new room', roomID);
+
+      var game = {
+        id: roomID,
+        host: socket.id,
+        challenge: challengeID
+      }
+
+      socket.adapter.rooms[roomID].game = game;
+    });
   });
 
+  socket.on('joinGame', function(roomID){
+    if (socket.adapter.rooms[roomID].length < 2) {
+      socket.join(roomID, function(){
+        socket.broadcast.emit('player joined', roomID);
+
+        socket.adapter.rooms[roomID].game.opponent = socket.id;
+      });
+    }
+    else {
+      console.log("NO");
+    }
+  });
 });
 
 http.listen(3000, function(){
