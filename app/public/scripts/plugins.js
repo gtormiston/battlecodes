a33a94f6efecc37dc5efd80c3cdbda8c73768085
Graham Ormiston <grahamormiston@gmail.com>
2016-07-21 13:33:39 +0100

    Incorporated codemirror syntax highlighting in game page

--- app/public/scripts/plugins.js
+++ app/public/scripts/plugins.js
@@ -1,5 +1,11 @@
-var myCodeMirror = CodeMirror.fromTextArea(document.getElementById("solution"), {
+var editor = CodeMirror.fromTextArea(document.getElementById("solution"), {
   lineNumbers: true,
   matchBrackets: true,
   mode: 'javascript'
+});
+// creates codemirror editor 
+
+
+$("#solution-form").submit(function(e){
+  var value = editor.getValue();
 });
