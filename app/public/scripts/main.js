390baa063d43e9464a8ed9dc283b54b08b378607
Graham Ormiston <grahamormiston@gmail.com>
2016-07-21 18:26:34 +0100

    feature additions and tweaks

--- app/public/scripts/main.js
+++ app/public/scripts/main.js
@@ -8,10 +8,12 @@
 
   socket.on('new room', function(data){
     $('#content').html($('#loading-template').html());
+    $('#room-id').text(data.roomID);
   });
 
   socket.on('roomsAvailable', function(data){
     $('#joinButtons').empty();
+    $('#joinButtons').append('<h2>Join a room</h2>');
     for (var i = 0; i < data.rooms.length; i++){
       $('#joinButtons').append('<button class="js-join-button">' + data.rooms[i] + '</button>');
     }
@@ -23,6 +25,7 @@
     $('#joinButtons').html($('#instructions-template').html());
     $('#js-instructions').text(data.instructions);
     socket.roomID = data.roomID;
+    console.log(socket.roomID);
     CodeMirror.fromTextArea(document.getElementById("solution"), {
       lineNumbers: true,
       matchBrackets: true,
@@ -62,9 +65,9 @@
   $("body").on('submit', '#solution-form', function(e){
     event.preventDefault();
     var submission = $('.js-solution-text').val();
-    var testResults = submissionHandler(submission,testCases,test,generateHTML,socket,playerName);
+    var testResults = submissionHandler(submission,testCases,test,generateHTML,playerName);
     $('.js-results').html(testResults);
   });
 
-  exports.init = socket;
+  exports.socket = socket;
 })(this);
