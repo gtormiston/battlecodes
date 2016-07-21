1ab41f2b79b096936e0eba9990a6edb9a37158c6
a-mellor <alexander.mellor@gmail.com>
2016-07-21 13:12:16 +0100

    implement codemirror

---
+++ app/public/scripts/plugins.js
@@ -0,0 +1,8 @@
+var myCodeMirror = CodeMirror.fromTextArea(document.getElementById("solution"), {
+  lineNumbers: true,
+  matchBrackets: true,
+  mode: 'javascript'
+});
+// myCodeMirror();
+// var textArea = document.getElementById("codeMirror");
+// var editor = CodeMirror.fromTextArea(textArea);
